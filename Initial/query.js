9d91acbd345a1c1c89e994f701f489b9fd5511a2
Jay <jai.desale20052005@gmail.com>
2022-04-08 23:49:56 +0530

    Update query.js

--- Initial/query.js
+++ Initial/query.js
@@ -1,7 +1,7 @@
 const db = require('gun')();
 
 
-const noderesult = db.get('63bd5c82-2591-4655-bc78-519a3b787063-$$UU$SSEE%%RRH@@TEESN0RE(COV@RYY').once(v =>{
+const noderesult = db.get('Enter your desired URK/UAK value').once(v =>{
         console.log(v)
      
         });
