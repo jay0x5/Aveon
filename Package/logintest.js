951fedeee5fa0578c8e861c80fe1a158b9013514
Jay <jai.desale20052005@gmail.com>
2022-04-12 10:32:44 +0530

    minor update

--- Package/logintest.js
+++ Package/logintest.js
@@ -1,8 +1,8 @@
 //Testing Login
 const {Loginuser} = require("./index.js")
 const UUID = String("31444335-3730-3451-5744-6C02E07DD074")
-var var1 = Loginuser("U2FsdGVkX19AiCwU5lH3AZkOFq7Q2vZ7KUZOV9XvWVySsJIOBouISXpHEqal+VlD8AXbjHhxlTbEd6fyQws9Eo4u7vK5538BvWtEgs621oLxDXvAknB7jDbe2eC20oIi",UUID,2,30)
-//encrypted CAT,UUID,UUIDparameter to slice UUID from, parameter to slice UUID to
+var var1 = Loginuser("U2FsdGVkX19AiCwU5lH3AZkOFq7Q2vZ7KUZOV9XvWVySsJIOBouISXpHEqal+VlD8AXbjHhxlTbEd6fyQws9Eo4u7vK5538BvWtEgs621oLxDXvAknB7jDbe2eC20oIi",UUID,"2","30")
+//encrypted CAT,UUID,UUIDparameter to slice UUID from[can be an int or a string], parameter to slice UUID to[can be an int or a string]
 
 var1.then(x=>
     {
--- Package/test.js
+++ Package/test.js
@@ -3,7 +3,7 @@
 
 const {RegisterUser} = require("./index.js")
 var rec = "$WKR@I%"
-var registres = RegisterUser("WSDIC1DQWD","P@wdqwd","M123123s12","31444335-3730-3451-5744-6C02E07DD074","2","30",rec)
+var registres = RegisterUser("WSDIC1DQWD","P@wdqwd","M123123s12","31444335-3730-3451-5744-6C02E07DD074",2,30,rec)
 
 //retrieving the returned CAT and URK
 registres.then(x=>
@@ -13,6 +13,6 @@ registres.then(x=>
     
     
     })
-//username,password,mail,UUID,parameter to slice UUID from, parameter to slice UUID to, RECOVERY SECRET
+//username,password,mail,UUID,parameter to slice UUID from[can be an int or a string], parameter to slice UUID to[can be an int or a string], RECOVERY SECRET
 
 
