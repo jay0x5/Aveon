9819f5ba63b0e998a4dc0bebe900e35f5dac9259
Jay <jai.desale20052005@gmail.com>
2022-04-16 13:16:30 +0530

    tested delete function

---
+++ Package/delete.js
@@ -0,0 +1,4 @@
+const aveon = require("./index.js")
+const UUID = String("31444335-3730-3451-5744-6C02E07DD074")
+const encat = "U2FsdGVkX18N/UOyq9rZ9EYqXqgROCY1ZjcyRqTpxBdMH2hd1+mQ3oDm4WS8xS4aQylrogtFtHCgNzTZlf3FbuNmhAnWaCUBcLj2RoSaI8AP/Q0Lsx8fS6OeKhQKE4BS"
+var var1 = aveon.DeleteUser(encat,UUID,"2","30")
