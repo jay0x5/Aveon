4d063b2ac53302832291f32f9384ab89cbff795c
Jay <jai.desale20052005@gmail.com>
2022-04-16 13:25:27 +0530

    Update delete.js

--- Package/delete.js
+++ Package/delete.js
@@ -1,4 +1,14 @@
 const aveon = require("./index.js")
+
 const UUID = String("31444335-3730-3451-5744-6C02E07DD074")
+
 const encat = "U2FsdGVkX18N/UOyq9rZ9EYqXqgROCY1ZjcyRqTpxBdMH2hd1+mQ3oDm4WS8xS4aQylrogtFtHCgNzTZlf3FbuNmhAnWaCUBcLj2RoSaI8AP/Q0Lsx8fS6OeKhQKE4BS"
-var var1 = aveon.DeleteUser(encat,UUID,"2","30")
+
+const CUTUUIDFROM = 2
+
+const CUTUUIDTO = 30
+var var1 = aveon.DeleteUser(encat,UUID,CUTUUIDFROM,CUTUUIDTO)
+
+result = var1.then(x=>{
+    console.log(x)
+})
