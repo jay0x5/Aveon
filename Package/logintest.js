446699ceb6a3070b0de1c075a0a3e42ba6fa287c
Jay <jai.desale20052005@gmail.com>
2022-04-19 19:44:51 +0530

    Update logintest.js

--- Package/logintest.js
+++ Package/logintest.js
@@ -1,7 +1,7 @@
 //Testing Login
 const {LoginUser} = require("./index.js")
 const UUID = String("31444335-3730-3451-5744-6C02E07DD074")
-var var1 = LoginUser('U2FsdGVkX18N/UOyq9rZ9EYqXqgROCY1ZjcyRqTpxBdMH2hd1+mQ3oDm4WS8xS4aQylrogtFtHCgNzTZlf3FbuNmhAnWaCUBcLj2RoSaI8AP/Q0Lsx8fS6OeKhQKE4BS',UUID,"2","30")
+var var1 = LoginUser('U2FsdGVkX1/xfmfFlWfvq0XN2DCSD4/e21bqmyQdecdCV9pR4Nt4oWIrp0Zk9muK40j8suD8kZ2wO3SEZazJSRBeTHEoBFeUn5yAVVa/tIM0NI4VXH1rqHS0NkuU8QM/',UUID,"2","30")
 //encrypted CAT,UUID,UUIDparameter to slice UUID from[can be an int or a string], parameter to slice UUID to[can be an int or a string]
 
 var1.then(x=>
