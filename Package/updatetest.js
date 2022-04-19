6fced78ce022bbe820a7e2eedcc4d5e4db7f0fa0
Jay <jai.desale20052005@gmail.com>
2022-04-19 19:44:34 +0530

    Update updatetest.js

--- Package/updatetest.js
+++ Package/updatetest.js
@@ -4,14 +4,14 @@ const Aveon = require("./index.js")
 const UUID = "31444335-3730-3451-5744-6C02E07DD074"
 
 //testing if username can be updated
-var UpdateuName = Aveon.UpdateUserName('NUSSS213sws12','U2FsdGVkX18N/UOyq9rZ9EYqXqgROCY1ZjcyRqTpxBdMH2hd1+mQ3oDm4WS8xS4aQylrogtFtHCgNzTZlf3FbuNmhAnWaCUBcLj2RoSaI8AP/Q0Lsx8fS6OeKhQKE4BS',UUID,"2","30")
+var UpdateuName = Aveon.UpdateUserName('NU22SS3241S213sws12','U2FsdGVkX1/xfmfFlWfvq0XN2DCSD4/e21bqmyQdecdCV9pR4Nt4oWIrp0Zk9muK40j8suD8kZ2wO3SEZazJSRBeTHEoBFeUn5yAVVa/tIM0NI4VXH1rqHS0NkuU8QM/',UUID,"2","30")
 console.log(UpdateuName.then(x=>{
 
     console.log(x)
 }))
 
 //testing if mail can be updated
-var UpdateMail = Aveon.UpdateMail('NMAAY123269sws12','U2FsdGVkX18N/UOyq9rZ9EYqXqgROCY1ZjcyRqTpxBdMH2hd1+mQ3oDm4WS8xS4aQylrogtFtHCgNzTZlf3FbuNmhAnWaCUBcLj2RoSaI8AP/Q0Lsx8fS6OeKhQKE4BS',UUID,"2","30")
+var UpdateMail = Aveon.UpdateMail('N21eMqswsqw2AAY123269sws12','U2FsdGVkX1/xfmfFlWfvq0XN2DCSD4/e21bqmyQdecdCV9pR4Nt4oWIrp0Zk9muK40j8suD8kZ2wO3SEZazJSRBeTHEoBFeUn5yAVVa/tIM0NI4VXH1rqHS0NkuU8QM/',UUID,"2","30")
 console.log(UpdateMail.then(y=>{
 
     console.log(y)
