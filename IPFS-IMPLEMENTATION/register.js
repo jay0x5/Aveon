61895872a4fb00745d5330cb83a09a36ffd99491
Jay <jai.desale20052005@gmail.com>
2022-04-21 18:34:22 +0530

    test register
    file to locally test module

---
+++ IPFS-IMPLEMENTATION/register.js
@@ -0,0 +1,5 @@
+const aveon = ('./index.js')
+
+obj = {username:"lid hqiw",PAASSWD:"d23dh 3 d3"}
+var op = aveon.RegisterUser(obj,"uuidbyjay12d12d2d12dh182d9129d2udzd129dz20d29dd","2","31","recsecx")
+console.log(op.then(x=>{console.log(x)}))
